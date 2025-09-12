c545db78ec1ef0e2b03327bee52cb272599df0eb
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2025-09-12 13:00:42 +0300

    more dropdown functionality and templates for pages

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -34,3 +34,32 @@ document.querySelectorAll("[data-include]").forEach(async (el) => {
     });
   }
 });
+
+document.querySelectorAll("[data-include]").forEach(async (el) => {
+  const path = el.getAttribute("data-include");
+  const res = await fetch(path);
+  el.innerHTML = await res.text();
+
+  if (path.includes("sidebar/sidebar.html")) {
+    const currentPath = window.location.pathname;
+    const menuItems = document.querySelectorAll(".sidebar-menu a");
+
+    console.log("Current Path:", currentPath);
+
+    menuItems.forEach((link) => {
+      if (link.getAttribute("href") === currentPath) {
+        console.log("Matching link found:", link);
+
+        const parentSubList = link.closest("#sidebar-main-item");
+        if (parentSubList) {
+          parentSubList
+            .querySelector(".sidebar-sub-list")
+            .classList.toggle("open");
+          parentSubList
+            .querySelector(".dropdown-toggle")
+            .classList.toggle("open");
+        }
+      }
+    });
+  }
+});
