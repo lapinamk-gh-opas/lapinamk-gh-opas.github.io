5fca197f614d09fe8cf61afb190084b858727534
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2025-09-12 09:51:22 +0300

    sidebar functionality tweaks and added dropdown functionality for submenus

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -8,15 +8,29 @@ document.querySelectorAll("[data-include]").forEach(async (el) => {
     const toggleBtn = document.getElementById("toggleBtn");
     const mainContainer = document.getElementById("mainContainer");
 
-    console.log("Sidebar script loaded", { sidebar, toggleBtn, mainContainer });
-
     if (toggleBtn) {
       toggleBtn.addEventListener("click", () => {
-        console.log("Toggle sidebar");
         sidebar.classList.toggle("closed");
         toggleBtn.classList.toggle("closed");
         mainContainer.classList.toggle("shifted");
       });
     }
+  }
+});
+
+document.querySelectorAll("[data-include]").forEach(async (el) => {
+  const path = el.getAttribute("data-include");
+  const res = await fetch(path);
+  el.innerHTML = await res.text();
+
+  if (path.includes("sidebar/sidebar.html")) {
+    const listItems = document.querySelectorAll("#sidebar-main-item");
+    listItems.forEach((item) => {
+      const toggle = item.querySelector(".dropdown-toggle");
+      toggle.addEventListener("click", () => {
+        toggle.classList.toggle("open");
+        item.querySelector(".sidebar-sub-list").classList.toggle("open");
+      });
+    });
   }
 });
