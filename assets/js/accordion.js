23306f0e6a69d714ddb4ea921dc8310ddd8445e6
Antti Kortelainen <antti.kortelainen@edu.lapinamk.fi>
2025-09-24 15:01:43 +0300

    Add default open functionality for accordion elemets

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -2,7 +2,6 @@ document.addEventListener("subContent:loaded", () => {
   document.querySelectorAll(".accordion[data-include]").forEach(async (acc) => {
     const path = acc.dataset.include;
     const placeholder = acc.querySelector(".contentToAccordion");
-
     const original = placeholder ? placeholder.outerHTML : "";
 
     try {
@@ -10,6 +9,7 @@ document.addEventListener("subContent:loaded", () => {
       if (!resp.ok) throw new Error(resp.statusText);
 
       const html = await resp.text();
+
       acc.innerHTML = html;
 
       acc.dataset.originalContent = original;
@@ -33,10 +33,22 @@ document.addEventListener("accordion:loaded", (e) => {
     content.innerHTML = item.dataset.originalContent;
   }
 
+  const placeholderDiv = item.querySelector(".contentToAccordion");
+  const defaultOpen = placeholderDiv.classList.contains("open");
+  console.log("default open: ", defaultOpen);
+
   const imgOpen = item.querySelector(".bookButton.open");
   const imgClose = item.querySelector(".bookButton.close");
 
+  if (defaultOpen) {
+    content.classList.remove("hidden");
+    content.style.maxHeight = content.scrollHeight + "px";
+    imgOpen.classList.add("hidden");
+    imgClose.classList.remove("hidden");
+  }
+
   item.onclick = () => {
+    defaultOpen && placeholderDiv.classList.remove("open");
     const hidden = content.classList.contains("hidden");
 
     hidden
@@ -44,7 +56,7 @@ document.addEventListener("accordion:loaded", (e) => {
       : (content.style.maxHeight = "0px");
 
     content.classList.toggle("hidden", !hidden);
-    imgOpen?.classList.toggle("hidden", hidden);
-    imgClose?.classList.toggle("hidden", !hidden);
+    imgOpen.classList.toggle("hidden", hidden);
+    imgClose.classList.toggle("hidden", !hidden);
   };
 });
