3d1be8346e80977f22961a98152bfc9a296ca963
AnttiK87 <antti.kortelainen@edu.lapinamk.fi>
2025-11-05 14:41:03 +0200

    Revert "enable code format according to .prettierrc-file settings to all files in project for unified code formatting"
    This reverts commit 1d65a2420b8e05a390524d1b6c70c82488b1f7d4.

--- assets/js/loaderInit.js
+++ assets/js/loaderInit.js
@@ -1,12 +1,12 @@
-import { loadHead } from '/assets/js/includeHead.js';
+import { loadHead } from "/assets/js/includeHead.js";
 
 export async function initLoader(pageTitle) {
-  const loader = document.getElementById('loader');
-  loader.style.display = 'flex';
+  const loader = document.getElementById("loader");
+  loader.style.display = "flex";
 
-  await loadHead('/utils/head.html', pageTitle);
+  await loadHead("/utils/head.html", pageTitle);
   await document.fonts.ready;
 
-  loader.classList.add('fade-out');
-  document.dispatchEvent(new Event('content:loaded'));
+  loader.classList.add("fade-out");
+  document.dispatchEvent(new Event("content:loaded"));
 }
